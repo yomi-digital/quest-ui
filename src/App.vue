19ef862ea0eb7075d26b379f2aa9367ef550ffa8
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-05-18 17:52:26 +0200

    min fix

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav" style="text-align:right">
-      <a href="https://twitter.com/YOMI_WEB3" target="_blank" style="color:green; margin-right:30px">FOLLOW US</a><!-- |
+    <div id="nav" style="text-align: right">
+      <a
+        href="https://twitter.com/YOMI_WEB3"
+        target="_blank"
+        style="color: green; margin-right: 30px"
+        >FOLLOW US</a
+      ><!-- |
       <router-link to="/about">About</router-link> |
       <router-link to="/leaderboard">Leaderboard</router-link>-->
     </div>
-    <router-view style="margin-top:40px" />
-    <div class="collection external terminal" style="width:100%; background:#000; text-align:right; color: green">
-      Made with ❤️ at <a href="https://yomi.digital" style="color:green">YOMI</a>
+    <router-view style="margin-top: 40px" />
+    <div
+      class="collection external terminal"
+      style="width: 100%; background: #000; text-align: right; color: green"
+    >
+      Made with ❤️ at
+      <a href="https://yomi.digital" style="color: green">YOMI</a>
     </div>
   </div>
 </template>
@@ -19,6 +28,13 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+
+#WEB3_CONNECT_MODAL_ID {
+  font-family: var(--font, monospace);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
 #nav {
   position: fixed;
   top: 0;
